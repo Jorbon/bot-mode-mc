c133ed965e6d248734be3a2af00a1a4ce3e19deb
Jorbon <ben.a.phillips@outlook.com>
2026-07-19 22:30:26 -0500

    initial files

---
+++ src/main/java/github/jorbon/bot_mode/BotModeClient.java
@@ -0,0 +1,30 @@
+package github.jorbon.bot_mode;
+
+import org.lwjgl.glfw.GLFW;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
+
+public class BotModeClient implements ClientModInitializer {
+    
+    @Override
+    public void onInitializeClient() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (key_run.wasPressed()) {
+                BotModeClient.bot_mode = true;
+                System.out.println("Run key pressed!");
+            }
+        });
+    }
+    
+    public static boolean bot_mode = false;
+    
+    
+    private static final KeyBinding.Category BOT_MODE_CATEGORY = KeyBinding.Category.create(Identifier.of("bot_mode"));
+    private static KeyBinding key_run = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.bot_mode.run", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, BOT_MODE_CATEGORY));
+    
+}
